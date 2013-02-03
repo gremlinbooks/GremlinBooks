098b91ceef344fd9ff21eefb0abbb251cedf13f6
David Jarmoluk <DavidJ@MAC-5268.local>
2013-02-03 15:40:08 -0600

    themeing and amazon api integration. basic isbn search

---
+++ app/controllers/search_controller.rb
@@ -0,0 +1,21 @@
+class SearchController < ApplicationController
+
+  def show
+    require 'amazon/ecs'
+
+    Amazon::Ecs.options = {
+        :associate_tag => 'concllabs-20',
+        :AWS_access_key_id => '0C0D1DMW0V4ZH49EZHR2',
+        :AWS_secret_key => 'trNYD6Ky2UzMtjI0y0zMPq3ke24LQbrVCMYQ/DBf'
+    }
+
+    @res = Amazon::Ecs.item_lookup(params[:search], :id_type => 'ISBN', :search_index => 'Books')
+  end
+
+  def index
+
+
+
+  end
+
+end
