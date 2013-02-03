23bf13218a1150e932243cc5923f6952cc3b0af0
David Jarmoluk <DavidJ@MAC-5268.local>
2013-02-03 12:30:19 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GremlinBooks::Application.config.secret_token = 'd6334e58dce1b5a348acaee8961126ca1dae781bb0db8537488305a5a4df9d494945195543553111f2d8a9bc193cf23dfa469aaa1b1c9c6db60676a5a14b6b5f'
