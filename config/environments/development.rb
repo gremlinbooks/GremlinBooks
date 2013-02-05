df5ba3a9e12e33635cdafde8efd5ae941aaa88e9
David Jarmoluk <djarmoluk@gmail.com>
2013-02-04 19:07:36 -0600

    changes for devise email

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -35,6 +35,16 @@ GremlinBooks::Application.configure do
   # Expands the lines which load the assets
   config.assets.debug = true
 
-  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.default_url_options = { :host => 'smtp.gmail.com' }
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+      address:              'smtp.gmail.com',
+      port:                 587,
+      domain:               'conclusivelabs.com',
+      user_name:            'david',
+      password:             'Paige111',
+      authentication:       'plain',
+      enable_starttls_auto: true  }
 
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,4 +64,17 @@ GremlinBooks::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+
+  config.action_mailer.default_url_options = { :host => 'smtp.gmail.com' }
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+      address:              'smtp.gmail.com',
+      port:                 587,
+      domain:               'conclusivelabs.com',
+      user_name:            'david',
+      password:             'Paige111',
+      authentication:       'plain',
+      enable_starttls_auto: true  }
+
 end
