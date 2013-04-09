5fedfe1b5ec525c30724de48bd1e4da42ef87df8
David Jarmoluk <djarmoluk@gmail.com>
2013-04-08 21:55:03 -0500

    contact form submission fix for comment field

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -3,7 +3,7 @@ class ContactMailer < ActionMailer::Base
   def contact_email(contact)
     @name = contact[:name]
     @email = contact[:email]
-    @comment = contact[:comment]
+    @comment = contact[:comments]
 
     mail(:from => @email, :to => "cnowotny@gremlinbooks.com, djarmoluk@gmail.com", :subject => "Gremlin Books - Contact Form Submitted")
   end
