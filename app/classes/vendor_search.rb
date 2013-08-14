3632db2cfd4cd1b2967c2030f06db02c19e6204b
David Jarmoluk <djarmoluk@gmail.com>
2013-08-13 22:39:00 -0500

    advertiser id updates for bookbyte and bookrenter

--- app/classes/vendor_search.rb
+++ app/classes/vendor_search.rb
@@ -245,7 +245,7 @@ class VendorSearch
       end
     end
 
-    buy_url = "http://www.jdoqocy.com/click-#{@cj_website_id}-10365124?url=" + ad_url
+    buy_url = "http://www.jdoqocy.com/click-#{@cj_website_id}-1427863?url=" + ad_url
 
     #best used
     if book_byte_response["InventoryInfo"]["Bookbyte_Offers"]["Best_Used"]["IsOfferAvailable"]
@@ -356,7 +356,7 @@ class VendorSearch
 
     # create the url for a user buy click
     cart_url = book_renter_response["response"]["book"]["add_to_cart_url"]
-    buy_url = "http://www.jdoqocy.com/click-#{@cj_website_id}-10737829?url=" + cart_url
+    buy_url = "http://www.jdoqocy.com/click-#{@cj_website_id}-2860182?url=" + cart_url
 
 
     # book renter response is kind of jacked
