209eb2a6fa6544fc1028fe48d6a37a061357cfaf
David Jarmoluk <djarmoluk@gmail.com>
2013-11-24 21:38:02 -0600

    removed user counts

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -12,7 +12,6 @@ class PagesController < ApplicationController
     @tenants = Tenant.all
     @search_counts = UserSearchLog.user_search_counts_by_customer
     @buy_counts = UserBuyLog.user_buy_counts_by_customer
-    @user_counts = User.user_counts_by_customer
   end
 
 end
