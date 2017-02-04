82ac119dd50771cbe22e723b45a5dee207c108ac
gremlinbooks <cnowotny@gremlinbooks>
2017-02-04 14:13:32 -0600

    image file size change

--- db/seeds.rb
+++ db/seeds.rb
@@ -36,7 +36,7 @@ Tenant.create(sub_domain: 'aoii',
 
 #Gremlin
 Tenant.create(sub_domain: 'www',
-    logo: 'Gremlin.png',
+    logo: 'gremlin_small.png',
     color: 'Default',
     title: 'Gremlin Books',
     amazon_access_key: 'AKIAJGEEG4RCTCF2DCHQ',
